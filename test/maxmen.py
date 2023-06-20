c1ad29249b4f07a056e6b013ceba5fa8bdf26f43
Vincent <fanvincent@hotmail.com>
2023-06-20 10:51:52 +0800

    GTCLU

--- test/maxmen.py
+++
@@ -1,8 +0,0 @@
-with open("./memeory.txt") as fl:
-    res = 0
-    line = fl.readline().strip()
-    while line:
-        var = int(line[:-1])
-        res = max(var, res)
-        print(res)
-        line = fl.readline().strip()
